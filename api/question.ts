78e59d4db9ade65b442a9e02193d910207d7f297
Aloyise Biju Mathew <aloyise.mathew@experionglobal.local>
2024-01-04 10:38:12 +0530

    fix:test json return

--- api/question.ts
+++ api/question.ts
@@ -1,6 +1,23 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import germanData from '../json-langs/german';
-export default function handler(req: VercelRequest, res: VercelResponse) {
+// import type { VercelRequest, VercelResponse } from '@vercel/node';
+// import "../json-langs/german_lev_1.json" as
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-    return res.json(germanData)
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    try {
+        const response = await fetch('../json-langs/german_lev_1.json');
+        console.log('first    ' + response);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log('end    ' + data);
+        return res.json(data);
+    } catch (error) {
+        console.error('Error:', error);
+
+        // Send a more detailed error response
+        return res.status(500).json({ error: 'Internal Server Error', details: error.message });
+    }
 }
