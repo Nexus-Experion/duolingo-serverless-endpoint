bf9a3a9c5abd0bbe586cb2a53c55f3ddc93d146c
Aloyise Biju Mathew <aloyise.mathew@experionglobal.local>
2024-01-04 10:08:48 +0530

    fix:test json return

--- api/question.ts
+++ api/question.ts
@@ -1,12 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    let langs = req.query.lang;
-    const response = await fetch('../json-langs/german_lev_1.json'); // Replace with your API endpoint
-    if (!response.ok) {
-        throw new Error('Failed to fetch data');
-    }
+    // let langs = req.query.lang;
+    const response = await fetch('../json-langs/german_lev_1.json');
+    fetch('../json-langs/german_lev_1.json')
+        .then(response => response.json())
+        .then(data => { return res.json(data) })
 
 
-    return res.json(response)
 }
