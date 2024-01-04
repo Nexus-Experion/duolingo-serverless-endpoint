2206d63542914cafe0264d6621afb8daabb26f96
Aloyise Biju Mathew <aloyise.mathew@experionglobal.local>
2024-01-04 09:49:38 +0530

    fix:test para passing

--- api/question.ts
+++ api/question.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-
-    return res.json(req.query)
+    let langs = req.query.lang;
+    return res.json(langs)
 }
