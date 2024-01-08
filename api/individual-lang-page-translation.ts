d992b839787ad956d2b061fd0a6502e728a7727a
Ashik George <ashik.george@experionglobal.local>
2024-01-08 22:45:23 +0530

    feat:added individual language page translation endpoint

--- api/individual-lang-page-translation.ts
+++ api/individual-lang-page-translation.ts
@@ -1,8 +1,9 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import germanData from '../questionaire-json/german.json' assert {type: 'json'};
-import spanishData from '../questionaire-json/spanish.json' assert {type: 'json'};
-import frenchData from '../questionaire-json/french.json' assert {type:'json'};
-import japaneseData from '../questionaire-json/japanese.json' assert {type:'json'};
+import germanData from '../individual-lang-page-jsons/german.json' assert {type: 'json'};
+import spanishData from '../individual-lang-page-jsons/spanish.json' assert {type: 'json'};
+import frenchData from '../individual-lang-page-jsons/french.json' assert {type:'json'};
+import japaneseData from '../individual-lang-page-jsons/japanese.json' assert {type:'json'};
+import englishData from '../individual-lang-page-jsons/english.json' assert {type:'json'};
 
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -12,8 +13,9 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
             case 'es': return res.json(spanishData)
             case 'fr': return res.json(frenchData)
             case 'ja': return res.json(japaneseData)
+            case 'en': return res.json(englishData)
             default: return res.status(400).send({
-                message:'This is an error!'
+                message:'No translation available. Sorry!'
             })
         }
 
@@ -24,5 +26,4 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         return res.status(500).json({ error: 'Internal Server Error', details: error.message });
     }
 }
-
 
